e21aba0e16f3946cfd1a2484c6a3079dd88313c5
MaríadelMarDP <158162256+Mariadelmardp@users.noreply.github.com>
2025-01-03 10:57:33 +0000

    archivo con datos data_limpios

--- src/soporte_analisis.py
+++ src/soporte_analisis.py
@@ -1,103 +1,43 @@
 import pandas as pd
 import numpy as np 
 
-def preanalisis(ruta_archivo):
-  df= cargar_datos(ruta_archivo)
-  set_opciones(df)
-  resumen_datos(df)
-  return df
-
-def cargar_datos(ruta_archivo):
-  """Cargar los datos desde un archivo CSV, Parquet o Excel
-
-  Args:
-      ruta_artivo 
-  """
-  # Cargar archivos
-  try:
-      if ruta_archivo.endwith(".csv"):
-        df = pd.read_csv(ruta_archivo)
-      elif ruta_archivo.endswith(".parquet"):
-        df= pd.read_parquet(ruta_archivo)
-      elif ruta_archivo.endswith()(".xlsx"):
-        df=pd.read_excel(ruta_archivo)
-      else:
-        raise ValueError ("Formato de archivo no soportado.")
-  except Exception as e:
-    print(f'Error al cargar los dtos: {e}')
-    return None
-  return df
-
-
-def set_opcion(df):
-  """ Normalizacionde texto
-
-  Args:
-      df 
-  """
-  pd.set_option('display.max.columns', None)
-  df.columns =(df.columns.str.strip().str.replace(' ','_').str.lower())
-  df = df.map(lambda x: x.strip().replace(' ', '_').lower() if isinstance(x, str) else x)
-  return df 
-
-def resumen_datos(df):
-  
-  """
-  
-  """
-  
-  #Resumen datos
-  print('\n-----------Resumen de datos----------- \n-----------')
-  print(f'El número de filas es: {df.shape[0]}\n----------')
-  print(f'El número de columnas es: {df.shape[1]}\n----------')
-  print('\nInformación general:')
-  display(df.info())
-  print('----------')
-  num_dupl= df.duplicated().sum()
-  print('El núemro de duplicados es: {num_dupl}')
-  if num_dupl != 0:
-    print(f'Eliminamos duplicados...')
-    df.drop_duplicates(inplace=True)
+def eda_preliminar(df):
+  display(df.sample(5))
+  print('________________')
+  print('INFO')
+  df.info()
+  print('________________')
+  print('NULOS')
+  display(round(df.isnull().sum()/df.shape[0]*100,2))
+  print('________________')
+  print('DUPLICADOS')
+  display(df.duplicated().sum())  
+  print('________________')
+  print('VALORES ÚNICOS')
+  for col in df.select_dtypes(include='O').columns:
+    print(df[col].value_counts())
+    print('_______________')
     
-  df_nulos = pd.DataFrame({'count':df.isnull().sum(), '%nulos': (df.isnull().sum() / df.shape[0].round(3) * 100)})
-  df_nulos=df_nulos[df_nulos['count'] >0]
-  df_nulos_sorted= df_nulos.sort_values(by='%nulos', ascending=False)
-  print('Los nulos que tenemos en el conjunto de datos son:')
-  display(df_nulos_sorted)
-  
-  columnas_por_tipo= {dtype: df.select_dtype(dtype).columns.tolist() for dtype in df.dtype.unique()}
-  print(f'Los tipos de las columnas son:')
-  display(pd.DataFrame(df.dtype, columns=['Tipo_dato']))
-  print('\nColumnas agrupadas por tipo de dato:')
-  for tipo, columnas in columnas_por_tipo.items():
-    print(f'- {tipo}:, columnas')
-  print('\n-----------------------------------------------\n')
-  identificar_columnas_booleanas(df)
-  print('---------------')
-  display(df.head())
-  
-  return df
+def valores_minuscula(df):
+  for col in df.select_dtypes(include='O').columns:
+    df[col] = df[col].str.lower() 
+    
+def dolares (df, lista_col):
+  for col in lista_col:
+    df[col]=df[col].str.replace('$', ' ').str.replace(',', '')      
 
-def identificar_columnas_booleanas(df):
-  
-  posibles_booleanas=[col for col in df.columns if set(df[col].dropna().unique()) <={0,1}]
-  
-  print(f'Columnas que podrían ser booleanas: {posibles_booleanas}' )
-  return posibles_booleanas
-
-
-def cambiar_tipo (df, formato_fecha):
+def comas (df, lista_col):
+  for col in lista_col:
+    df[col]=df[col].str.replace(',', '.')
+    
+def convertir_columnas (df, formato_fecha):
   for col in df.columns:
     for dtype in [float, int]:
       try:
-        df[col]=df[col].astype(dtype)
+       df[col]=df[col].astype(dtype)
       except:
-        pass
-    try: 
-      df[col] =pd.to_datetime(df[col], format=formato_fecha)
-    except:
-      pass               
-
-def expan_fecha()
-
-
+       pass
+    try:
+       df[col]=pd.to_datetime(df[col], format=formato_fecha)
+    except: 
+      pass
